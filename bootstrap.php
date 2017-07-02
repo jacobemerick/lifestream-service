65119855ed1f814903533875dea91909740a57b6
jacobemerick <jpemeric@gmail.com>
2017-07-01 22:05:31 -0700

    Test endpoint for types

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,7 @@ $startMemory = memory_get_usage();
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Aura\Di\ContainerBuilder;
 use AvalancheDevelopment\Talus\Talus;
 
 // load the config for the application
@@ -27,61 +28,6 @@ if ($last_json_error !== JSON_ERROR_NONE) {
 
 $builder = new ContainerBuilder();
 $di = $builder->newInstance($builder::AUTO_RESOLVE);
-
-// set up db and models
-$di->set('dbal', $di->lazyNew(
-    'Aura\Sql\ExtendedPdo',
-    (array) $config->database
-));
-$di->types['Aura\Sql\ExtendedPdo'] = $di->lazyGet('dbal');
-
-$di->set('commentModel', $di->lazyNew('Jacobemerick\CommentService\Model\Comment'));
-$di->set('commentBodyModel', $di->lazyNew('Jacobemerick\CommentService\Model\CommentBody'));
-$di->set('commentDomainModel', $di->lazyNew('Jacobemerick\CommentService\Model\CommentDomain'));
-$di->set('commentLocationModel', $di->lazyNew('Jacobemerick\CommentService\Model\CommentLocation'));
-$di->set('commentPathModel', $di->lazyNew('Jacobemerick\CommentService\Model\CommentPath'));
-$di->set('commentRequestModel', $di->lazyNew('Jacobemerick\CommentService\Model\CommentRequest'));
-$di->set('commentThreadModel', $di->lazyNew('Jacobemerick\CommentService\Model\CommentThread'));
-$di->set('commenterModel', $di->lazyNew('Jacobemerick\CommentService\Model\Commenter'));
-
-// set up serializers
-$di->set('commentSerializer', $di->lazyNew('Jacobemerick\CommentService\Serializer\Comment'));
-$di->set('commenterSerializer', $di->lazyNew('Jacobemerick\CommentService\Serializer\Commenter'));
-
-// set up notification handler
-$di->set('notificationHandler', $di->lazyNew(
-    'Jacobemerick\CommentService\Helper\NotificationHandler',
-    [
-        'mail' => $di->lazyGet('mail'),
-        'commenterModel' => $di->lazyGet('commenterModel'),
-    ]
-));
-
-// set up logger
-$di->set('logger', $di->lazyNew(
-    'Monolog\Logger',
-    [
-        'name' => 'default',
-    ],
-    [
-        'pushHandler' => $di->lazyNew(
-            'Monolog\Handler\StreamHandler',
-            [
-                'stream' => __DIR__ . '/logs/default.log',
-                'level' => Monolog\Logger::DEBUG,
-            ]
-        ),
-    ]
-));
-
-// set up mailer
-$di->set('mail', $di->lazyNew(
-    'Jacobemerick\Archangel\Archangel',
-    [],
-    [
-        'setLogger' => $di->lazyGet('logger'),
-    ]
-));
 
 // global time object
 $di->set('datetime', new DateTime(
@@ -106,37 +52,10 @@ if (json_last_error() !== JSON_ERROR_NONE) {
 $talus = new Talus($swagger);
 
 // controllers
-use Jacobemerick\CommentService\Controller;
+use Jacobemerick\LifestreamService\Controller;
 
-$talus->addController('createComment', function ($req, $res) use ($di) {
-    return (new Controller\Comment($di))->createComment($req, $res);
+$talus->addController('getTypes', function ($req, $res) use ($di) {
+    return (new Controller\Type($di))->getTypes($req, $res);
 });
-$talus->addController('getComment', function ($req, $res) use ($di) {
-    return (new Controller\Comment($di))->getComment($req, $res);
-});
-$talus->addController('getComments', function ($req, $res) use ($di) {
-    return (new Controller\Comment($di))->getComments($req, $res);
-});
-
-$talus->addController('getCommenter', function ($req, $res) use ($di) {
-    return (new Controller\Commenter($di))->getCommenter($req, $res);
-});
-$talus->addController('getCommenters', function ($req, $res) use ($di) {
-    return (new Controller\Commenter($di))->getCommenters($req, $res);
-});
-
-// middleware
-use Jacobemerick\CommentService\Middleware;
-
-$talus->addMiddleware(new Middleware\Authentication(
-    $config->auth->username,
-    $config->auth->password
-));
 
 $talus->run();
-
-$di->get('logger')->addInfo('Runtime stats', [
-    'request' => $_SERVER['REQUEST_URI'],
-    'time' => (microtime(true) - $startTime),
-    'memory' => (memory_get_usage() - $startMemory),
-]);
--- src/Controller/Type.php
+++ src/Controller/Type.php
@@ -2,10 +2,36 @@
 
 namespace Jacobemerick\LifestreamService\Controller;
 
+use Interop\Container\ContainerInterface as Container;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 class Type
 {
 
-    public function __construct() {}
+    /** @var Container */
+    protected $container;
 
-    public function getTypes() {}
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function getTypes(Request $request, Response $response)
+    {
+        $types = [
+            'message' => 'hello world',
+        ];
+        $types = json_encode($types);
+        $response->getBody()->write($types);
+        return $response;
+    }
 }
