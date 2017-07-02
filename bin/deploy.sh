c87d2d4d7a513fa7a7166946d1d7d2688150c5cb
jacobemerick <jpemeric@gmail.com>
2017-07-02 07:03:33 -0700

    Deploy flow - test

---
+++ bin/deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
+
+  echo "Clears git information"
+  rm -rf .git
+
+  echo "Rebuilds dependencies"
+  rm -rf vendor
+  composer install --no-dev --no-interaction
+
+  echo "Writing custom gitignore for build"
+  echo "# Build Ignores" > .gitignore
+  echo "composer.phar" >> .gitignore
+  echo "config.json" >> .gitignore
+  echo "deploy_key.*" >> .gitignore
+  echo "build/" >> .gitignore
+  echo "codeclimate.json" >> .gitignore
+  echo "coverage.xml" >> .gitignore
+
+  echo "Sets up package for sending"
+  git init
+  git remote add deploy $DEPLOY_URI
+  git config user.name $DEPLOY_USER
+  git config user.email $DEPLOY_EMAIL
+  git add --all .
+  git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
+
+  echo "Sets up permissions"
+  echo -e "Host lifestream.reynrick.com\n\tStrictHostKeyChecking no" >> ~/.ssh/config
+  openssl aes-256-cbc -K $encrypted_9ad63a373c60_key -iv $encrypted_9ad63a373c60_iv -in deploy_key.pem.enc -out deploy_key.pem -d
+  eval "$(ssh-agent -s)"
+  chmod 600 deploy_key.pem
+  ssh-add deploy_key.pem
+
+  echo "Sends build"
+  git push -f deploy master
+
+fi
