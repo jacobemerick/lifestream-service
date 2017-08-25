ee671ce85317c40d36e76a3ffd0ae2e33fbcc685
jacobemerick <jpemeric@gmail.com>
2017-08-24 20:41:37 -0700

    Layout of twitter api requests

--- src/Cron/Twitter.php
+++ src/Cron/Twitter.php
@@ -2,6 +2,9 @@
 
 namespace Jacobemerick\LifestreamService\Cron;
 
+use Exception;
+
+use Abraham\TwitterOAuth\TwitterOAuth as Client;
 use Interop\Container\ContainerInterface as Container;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
@@ -24,5 +27,43 @@ class Twitter implements CronInterface, LoggerAwareInterface
 
     public function run()
     {
+        $page = 1;
+
+        try {
+            $tweets = $this->fetchTweets(
+                $this->container->get('twitterClient'),
+                $this->container->get('config')->twitter->screenname
+            );
+        } catch (Exception $e) {
+            $this->logger->error($exception->getMessage());
+            return;
+        }
+
+        if (empty($tweets)) {
+            return;
+        }
+
+        $this->logger->debug("Processing page {$page} of tweets");
+
+        // process tweets
+        // repeat request w/ last id
+    }
+
+    protected function fetchTweets(Client $client, $screenname, $maxId = null)
+    {
+        $params = [
+            'screen_name' => $screenname,
+            'count' => 200,
+            'trim_user' => true,
+        ];
+        if (!is_null($maxId)) {
+            $params['max_id'] = $maxId;
+        }
+
+        $tweets = $client->get('statuses/user_timeline', $params);
+        if ($client->getLastHttpCode() !== 200) {
+            throw new Exception("Error with fetching tweets: {$client->getLastHttpCode()}");
+        }
+        return $tweets;
     }
 }
