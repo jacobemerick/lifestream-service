f111abb65203b7c471cb70b759bbf8e9cc143875
jacobemerick <jpemeric@gmail.com>
2017-09-13 15:16:06 -0700

    Adds new photo table to schema def

--- db/schema.sql
+++ db/schema.sql
@@ -110,6 +110,21 @@ SHOW WARNINGS;
 
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `photo`;
+
+CREATE TABLE `photo` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `media_id` int(11) unsigned NOT NULL,
+  `datetime` datetime NOT NULL,
+  `metadata` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SHOW WARNINGS;
+
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `twitter`;
 
 CREATE TABLE `twitter` (
